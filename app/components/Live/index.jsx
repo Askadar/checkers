bda842d55e623fea41e5977097959fa8ba3466dc
Azriel <askadar.ru@gmail.com>
2016-10-17 10:38:07 +0300

    Updates

--- app/components/Live/index.jsx
+++ app/components/Live/index.jsx
@@ -6,7 +6,7 @@ class Live extends React.Component {
 		return (
 			<div id="Live">
 				<ul className="list-group">
-					<Match first="Kunde" second="Pirken" timePlayed="12:22"/>
+					<Match first="Человек" second="Человек" timePlayed="12:22"/>
 				</ul>
 			</div>
 		)
