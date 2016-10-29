ca924929769fb557e8c90ec71308e6e4b532169a
Azriel <askadar.ru@gmail.com>
2016-10-29 23:06:11 +0300

    Working socks and viewing games, basic rooms support
    TODO: make some way to identify rooms on clients side

--- index.js
+++ index.js
@@ -2,30 +2,41 @@
 // var express = require('express');
 // var app = express();
 // var server = require('http').createServer(app);
-'use strict';
+"use strict";
 
-
-
-
-var cors_proxy = require('cors-anywhere');
-let proxy = cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-})
-
-var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
-var io = require('socket.io')(proxy);
+var io = require('socket.io')();
 var port = process.env.PORT || 3000;
 var uid = require('uid-safe');
 var uuid = require('uuid');
 class Room {
-	constructor(){
-		this.id = uuid.v4();
+	constructor(socket, debugUid = uuid.v4()){
+		this.id = debugUid;
 		this.users = [];
+		this.playing = [];
+		this.moves = [];
+		this.join(socket);
 	}
 	toString(){
 		return this.id;
+	}
+	join(socket){
+		this.users.push(socket);
+		this.playing.length < 2 && this.playing.push(socket);
+		socket.join(this.id);
+		socket.room = this;
+		socket.emit('meta','successfully joined room'+this)
+		socket.emit('moves', this.moves);
+		console.log('onJoin log',this);
+	}
+	leave(socket){
+		if (this.players.some(p=>p.id == socket.id)){
+			// TODO: wait for them to reconnect and don't throw around users array
+		}
+		this.users = this.users.filter(u=>u.id != socket.id);
+
+	}
+	pushMove(move){
+		this.moves.push(move);
 	}
 }
 
@@ -34,16 +45,47 @@ var rooms = []; //holds all active rooms ids or maybe room objects
 io.on('connection', function (socket) {
 	//setInterval(()=>socket.emit('move','testing stuff'), 1000);
 	socket.uid = uuid.v4();
+	socket.on('reconnect', function (nr) {
+	    console.log('reconnected, nr: ', nr);
+	});
+	//disconnect event
+	socket.on('disconnect', function () {
+	    console.log('disconnected', socket.id, rooms, socket.room);
+		//	socket.room.leave(socket);
+	});
 	socket.on('enterRoom', data =>{
-		if (rooms.some(room => room == data.id)){ //we have room already, we use just object cause rooms have overwritten toString method
-			rooms[data.id].join(socket)
+		console.log('onEnterRoom Handler', rooms, socket.room, data);
+		if (socket.room && rooms[socket.room]){ //we have room already, we use just object cause rooms have overwritten toString method
+		}
+		else {
+			if(data && data.id){
+				let index = -1;
+				if(rooms.some((room, i) => {index = i; return room == data.id})){
+					rooms[index].join(socket);
+				}
+				else
+					rooms.push(new Room(socket, data.id));
+			}
+			else{
+				rooms.push(new Room(socket));
+			}
 		}
 	})
   socket.on('move', data => {
     console.log(data);
+		socket.room.pushMove(data);
     socket.broadcast.emit('move',data);
   })
 });
 
+
+io.on('reconnect', function (nr) {
+		console.log('io reconnected, nr: ', nr);
+});
+//disconnect event
+io.on('disconnect', function () {
+		console.log('io disconnected');
+});
+//setInterval(()=>console.log(rooms),5000)
 io.listen(port);
 console.log('listening on port', port);
